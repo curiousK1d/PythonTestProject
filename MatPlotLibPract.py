9de5811683758bfc0e415d30b6e05c2418f29e37
Manoj <mpathak66@gmail.com>
2020-05-03 18:50:29 +0530

    matPlotLib Practice

--- MatPlotLibPract.py
+++ MatPlotLibPract.py
@@ -1,18 +1,17 @@
-
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
-x = np.arange(1,21,2)
-y = np.arange(20,0,-2)
-y2= np.arange(1,21,2)
+x = np.arange(1, 21, 2)
+y = np.arange(20, 0, -2)
+y2 = np.arange(1, 21, 2)
 
-print("\n Values of X : \n " +str(x))
-print("\n Values of Y : \n " +str(y))
+print("\n Values of X : \n " + str(x))
+print("\n Values of Y : \n " + str(y))
 
 # Line plot
-plt.plot(x,y,color="red",linewidth=3)
-plt.plot(x,y2,color="blue",linewidth=3)
+plt.plot(x, y, color="red", linewidth=3)
+plt.plot(x, y2, color="blue", linewidth=3)
 plt.title("Line Plot")
 plt.xlabel("X axis")
 plt.ylabel("y Axis")
@@ -20,25 +19,25 @@ plt.ylabel("y Axis")
 plt.show()
 
 # Bar Plot
-plt.bar(x,y)
+plt.bar(x, y)
 plt.show()
 
 # Scatter plot
 startup = pd.read_csv("resources\\50_Startups.csv")
-plt.scatter(startup["State"],startup["Administration"])
+plt.scatter(startup["State"], startup["Administration"])
 plt.show()
 
-#Histogram
+# Histogram
 plt.hist(startup["Administration"])
 plt.show()
 
-#Boxplot
+# Boxplot
 
-plt.show(startup.boxplot(column="R&D Spend",by="State"))
-plt.show(startup.boxplot(column="Profit",by="State"))
+plt.show(startup.boxplot(column="R&D Spend", by="State"))
+plt.show(startup.boxplot(column="Profit", by="State"))
 
-
-#Seaborn
+# Seaborn
 import seaborn as sb
-plt.show(sb.boxplot(x=startup["State"],y=startup["Profit"]))
-plt.show(sb.boxplot(startup["State"],startup["R&D Spend"]))
+
+plt.show(sb.boxplot(x=startup["State"], y=startup["Profit"]))
+plt.show(sb.boxplot(startup["State"], startup["R&D Spend"]))
